c589702615f332fe05cb9c017506d3f104a23ce5
Alexandra <alexandra.mille-egea@epfedu.fr>
2022-11-29 15:30:15 +0100

    google

--- first_micro/main.py
+++ first_micro/main.py
@@ -4,6 +4,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World"
+
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=G-XE21ZZ8ZV7"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-XE21ZZ8ZV7');
+    </script>
+    """
+    return prefix_google + "Hello World"
 
 # https://gscx4f.deta.dev
