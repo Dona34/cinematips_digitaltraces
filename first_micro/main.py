d28eabf3ee5aed9f668dca5391599d5469d74be4
Alexandra <alexandra.mille-egea@epfedu.fr>
2022-12-19 14:24:57 +0100

    render_template

--- first_micro/main.py
+++ first_micro/main.py
@@ -1,35 +1,35 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
-def hello_world():
+def index():
+    return render_template('index.html')
 
-    prefix_google = """
-    <!-- Google tag (gtag.js) -->
-    <script async
-    src="https://www.googletagmanager.com/gtag/js?id=G-XE21ZZ8ZV7"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-XE21ZZ8ZV7');
-    </script>
-    """
-    return prefix_google + "Hello World"
+# def hello_world():
+
+#     prefix_google = """
+#     <!-- Google tag (gtag.js) -->
+#     <script async
+#     src="https://www.googletagmanager.com/gtag/js?id=G-XE21ZZ8ZV7"></script>
+#     <script>
+#     window.dataLayer = window.dataLayer || [];
+#     function gtag(){dataLayer.push(arguments);}
+#     gtag('js', new Date());
+#     gtag('config', 'G-XE21ZZ8ZV7');
+#     </script>
+#     """
+#     return prefix_google + "Hello World"
 
 # https://gscx4f.deta.dev
 
-@app.route('/text', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+# @app.route('/text/', methods=['POST'])
+# def my_form_post():
+#     text = request.form['text']
+#     processed_text = text.upper()
+#     return processed_text
 
-@app.route('/logger')
+@app.route('/logger/', methods = ["GET"])
 def logger():
-
-    return print(app.logger)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('logger.html')
